b3dc91abb2a4db802dd84c4e4dfdc07be64482bf
lasercata <67599917+lasercata@users.noreply.github.com>
2025-06-03 23:29:17 +0200

    Adding conversation swagger

--- backend/api/routes/conversations.js
+++ backend/api/routes/conversations.js
@@ -7,7 +7,18 @@ const express = require('express');
 const router = express.Router();
 const conversationController = require('../controllers/conversationController');
 
-// route GET /api/conversations
+/**
+ * @swagger
+ * /api/conversations:
+ *   get:
+ *     summary: Get all the conversations
+ *     description: Get all the conversations
+ *     responses:
+ *       '200':
+ *         description: successful operation
+ *       '500':
+ *         description: internal error
+ */
 router.get('/', conversationController.getAllConversations);
 
 // route GET /api/conversations/analytics
